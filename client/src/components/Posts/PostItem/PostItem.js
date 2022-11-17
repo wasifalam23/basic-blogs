621bccdbbe5cb5dae74ec2dc40150488f2e4fd39
wasifalam23 <wsfalam84@gmail.com>
2022-11-17 21:55:27 +0530

    worked on postItem styles

--- client/src/components/Posts/PostItem/PostItem.js
+++ client/src/components/Posts/PostItem/PostItem.js
@@ -12,29 +12,34 @@ const PostItem = () => {
       </header>
       <main className="post-item__content--main">
         <p className="post-item__pub-date">Published on 22 Jan 2021</p>
-        <h3 className="post-item__title">Learning to code</h3>
+        <h3 className="post-item__title">
+          Learning to code for sudents to fight for this object
+        </h3>
         <p className="post-item__description">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolor,
           error aperiam in tempore ipsam reiciendis soluta sapiente sit
-          quibusdam.
+          quibusdam. error aperiam in tempore ipsam reiciendis soluta sapiente
+          sit quibusdam. quibusdam. error aperiam in tempore ipsam reiciendis
+          soluta sapiente sit quibusdam.
         </p>
-        <button>Read More</button>
-      </main>
-      <footer className="post-item__footer">
-        <div className="post-item__author--box">
-          <img
-            className="post-item__author--avatar"
-            src={joseph}
-            alt="author"
-          />
-          <p className="post-item__author--name">Patrick Davies</p>
-        </div>
+        <button className="post-item__read-more--btn">Read More</button>
 
-        <div className="post-item__comment--box">
-          <span className="post-item__comment--count">5</span>
-          <p className="post-item__comment--text">Comment</p>
-        </div>
-      </footer>
+        <footer className="post-item__footer">
+          <div className="post-item__author--box">
+            <img
+              className="post-item__author--avatar"
+              src={joseph}
+              alt="author"
+            />
+            <p className="post-item__author--name">Patrick Davies</p>
+          </div>
+
+          <div className="post-item__comment--box">
+            <span className="post-item__comment--count">5</span>
+            <p className="post-item__comment--text">Comments</p>
+          </div>
+        </footer>
+      </main>
     </div>
   );
 };
