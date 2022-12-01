c49c4e474ed7489a0535253c4dce0ad737a220af
wasifalam23 <wsfalam84@gmail.com>
2022-12-01 10:56:53 +0530

    added post comment

--- client/src/components/Posts/PostInfo/PostInfo.js
+++ client/src/components/Posts/PostInfo/PostInfo.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import useHttp from '../../../hooks/http-hook';
+import useForm from '../../../hooks/form-hook';
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
 import CommentList from '../Comment/CommentList/CommentList';
@@ -8,9 +9,24 @@ import Input from '../../FormElements/Input/Input';
 import Button from '../../FormElements/Button/Button';
 import './PostInfo.scss';
 
+const validateText = (value) => value.trim() !== '';
+
 const PostInfo = () => {
   const [post, setPost] = useState(null);
+  const [commentUpdated, setCommentUpdated] = useState(false);
+
   const { sendRequest: getPost } = useHttp();
+  const { sendRequest: createComment } = useHttp();
+
+  const {
+    value: enteredComment,
+    setEnteredValue: setComment,
+    valueChangeHandler: commentChangeHandler,
+    valueBlurHandler: commentBlurHandler,
+    isValid: commentIsValid,
+    hasError: commentHasError,
+    reset: resetComment,
+  } = useForm(validateText);
 
   const { id: postId } = useParams();
 
@@ -23,9 +39,40 @@ const PostInfo = () => {
       url: `http://localhost:3000/api/v1/blogs/${postId}`,
     };
     getPost(reqConfig, receivedData);
-  }, [getPost, postId]);
+  }, [getPost, postId, commentUpdated]);
 
   const pubDate = moment(post?.createdAt).format('Do MMM YYYY');
+
+  const token = localStorage.getItem('token');
+
+  const commentSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (!commentIsValid) return;
+
+    const createdCommentData = (data) => {
+      if (data.status === 'success') {
+        setCommentUpdated((prev) => !prev);
+        resetComment();
+      } else {
+        console.log(data);
+      }
+    };
+
+    const reqConfig = {
+      url: `http://localhost:3000/api/v1/blogs/${postId}/comments`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        comment: enteredComment,
+      }),
+    };
+
+    createComment(reqConfig, createdCommentData);
+  };
 
   return (
     <React.Fragment>
@@ -56,17 +103,35 @@ const PostInfo = () => {
             <h2 className="post-info__title">{post.title}</h2>
             <p className="post-info__descrp">{post.description}</p>
           </main>
+
           <section className="post-info__comment--section">
             <h3 className="post-info__comment--title">
               Comments ({post.comments.length})
             </h3>
             <CommentList comments={post.comments} />
-
-            <div className="post-info__comment--input-box">
-              <Input field="textarea" placeholder="Write your comment here" />
-              <Button type="button" className="post-info__comment--submit-btn">
-                Submit
-              </Button>
+            <div>
+              <form
+                onSubmit={commentSubmitHandler}
+                className="post-info__comment--form-control"
+              >
+                <Input
+                  field="textarea"
+                  id="comment"
+                  placeholder="Write your comment here"
+                  type="text"
+                  value={enteredComment}
+                  onChange={commentChangeHandler}
+                  onBlur={commentBlurHandler}
+                  hasError={commentHasError}
+                  errorMsg="Comment must not be empty"
+                />
+                <Button
+                  type="submit"
+                  className="post-info__comment--submit-btn"
+                >
+                  Submit
+                </Button>
+              </form>
             </div>
           </section>
         </main>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import postSlice from './post-slice';
 import authSlice from './auth-slice';
 import userSlice from './user-slice';
+import commentSlice from './comment-slice';
 
 const store = configureStore({
   reducer: {
     post: postSlice.reducer,
     auth: authSlice.reducer,
     user: userSlice.reducer,
+    comment: commentSlice.reducer,
   },
 });
 
