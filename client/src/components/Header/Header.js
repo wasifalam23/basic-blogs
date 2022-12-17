7ec071ccff348b21938a035b059a738456f2ec5a
wasifalam23 <wsfalam84@gmail.com>
2022-12-17 11:32:43 +0530

    added navdrawer for small device

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import MainNavigation from './MainNavigation/MainNavigation';
+import Backdrop from '../../utils/Modal/Backdrop/Backdrop';
 import './Header.scss';
 
 const Header = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const burgerClickHandler = () => {
+    setDrawerIsOpen((prev) => !prev);
+  };
+
+  const backdropClickHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <header className="header">
-      <Link to="/" className="header__logo">
-        Basic Blogs
-      </Link>
-      <MainNavigation />
+      <nav className="header__nav">
+        {drawerIsOpen && (
+          <Backdrop
+            className="header__backdrop"
+            onCancel={backdropClickHandler}
+          />
+        )}
+
+        <Link to="/" className="header__logo">
+          Basic Blogs
+        </Link>
+
+        <MainNavigation drawerIsOpen={drawerIsOpen} />
+
+        <div className="header__burger" onClick={burgerClickHandler}>
+          <div className="header__burger--line1 header__burger--item" />
+          <div className="header__burger--line2 header__burger--item" />
+          <div className="header__burger--line3 header__burger--item" />
+          <div />
+          <div />
+        </div>
+      </nav>
     </header>
   );
 };
--- client/src/components/Header/MainNavigation/MainNavigation.js
+++ client/src/components/Header/MainNavigation/MainNavigation.js
@@ -7,9 +7,9 @@ import NavUser from '../NavUser/NavUser';
 
 import './MainNavigation.scss';
 
-const MainNavigation = () => {
+const MainNavigation = (props) => {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const loggedInUserId = useSelector((state) => state.user.userData._id);
+  const loggedInUser = useSelector((state) => state.user.userData);
 
   const linkClass = ({ isActive }) => {
     return isActive
@@ -18,45 +18,78 @@ const MainNavigation = () => {
   };
 
   return (
-    <nav className="main-navigation">
-      <ul className="main-navigation__list">
-        <li className="main-navigation__list--item ">
-          <NavLink className={linkClass} to="/">
-            Posts
+    <ul
+      className={`main-navigation__list ${
+        props.drawerIsOpen && 'main-navigation__list--active'
+      }`}
+    >
+      {isLoggedIn && (
+        <div className="main-navigation__user--box">
+          <img
+            className="main-navigation__user--photo"
+            src={`http://localhost:3000/users/${loggedInUser.photo}`}
+            alt="user-avatar"
+          />
+
+          <div className="main-navigation__user--info">
+            <p className="main-navigation__user--name">
+              {loggedInUser.firstName} {loggedInUser.lastName}
+            </p>
+            <p className="main-navigation__user--email">{loggedInUser.email}</p>
+          </div>
+        </div>
+      )}
+
+      <li className="main-navigation__list--item">
+        <NavLink className={linkClass} to="/">
+          Posts
+        </NavLink>
+      </li>
+
+      {!isLoggedIn && (
+        <li className="main-navigation__list--item">
+          <NavLink className={linkClass} to="/auth">
+            Login
           </NavLink>
         </li>
+      )}
 
-        {!isLoggedIn && (
-          <li className="main-navigation__list--item">
-            <NavLink className={linkClass} to="/auth">
-              Login
-            </NavLink>
-          </li>
-        )}
+      {isLoggedIn && (
+        <li className="main-navigation__list--item">
+          <NavLink className={linkClass} to="/posts/new">
+            Add Post
+          </NavLink>
+        </li>
+      )}
 
-        {isLoggedIn && (
-          <li className="main-navigation__list--item">
-            <NavLink className={linkClass} to="/posts/new">
-              Add Post
-            </NavLink>
-          </li>
-        )}
+      {isLoggedIn && (
+        <li className="main-navigation__list--item">
+          <NavLink className={linkClass} to={`/${loggedInUser._id}/posts`}>
+            My Posts
+          </NavLink>
+        </li>
+      )}
 
-        {isLoggedIn && (
-          <li className="main-navigation__list--item">
-            <NavLink className={linkClass} to={`/${loggedInUserId}/posts`}>
-              My Posts
-            </NavLink>
-          </li>
-        )}
+      {isLoggedIn && (
+        <li className="main-navigation__list--item main-navigation__edit-profile">
+          <NavLink className={linkClass} to="/user-update">
+            Edit Profile
+          </NavLink>
+        </li>
+      )}
 
-        {isLoggedIn && (
-          <li className="main-navigation__list--item">
-            <NavUser />
-          </li>
-        )}
-      </ul>
-    </nav>
+      {isLoggedIn && (
+        <li className="main-navigation__list--item main-navigation__logout">
+          <button className="main-navigation__logout--btn">Logout</button>
+        </li>
+      )}
+
+      {isLoggedIn && (
+        <li className="main-navigation__list--item main-navigation__nav--user">
+          <NavUser />
+        </li>
+      )}
+    </ul>
   );
 };
 
